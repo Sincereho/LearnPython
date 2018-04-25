911710f3332e7a65b92742d68339f288391081d8
Sincere Hu <gogohxx123@gmail.com>
2018-04-25 21:36:36 +0800

    add  .gitignore and file 4_23-29

---
+++ 4_23-29/ex1.py
@@ -0,0 +1,7 @@
+print ("Hello World")
+print ("再次你好")
+print ("I like typing this.")
+print ("This is fun.")
+print ('Yay!Printing.')
+print ("I'd much rather you 'not'.")
+print ('I "said" do not touch this.')
