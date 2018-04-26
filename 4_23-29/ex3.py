db1410a9d2a7c3da902e2ea60d10b22d5a7e44ba
Sincere Hu <gogohxx123@gmail.com>
2018-04-27 00:10:36 +0800

    Completing EX3 additional exercises

--- 4_23-29/ex3.py
+++ 4_23-29/ex3.py
@@ -1,23 +1,23 @@
-print ("I will not count my chickens:")
+print ("I will now count my chickens:")
 
-print ("Hens", 25 + 30 / 6)
-print ("Roosters", 100 - 25 * 3 % 4)
+print ("Hens", 25 + 30 // 6)                     #输出 "母鸡数" 25+ 30/6
+print ("Roosters", 100 - 25 * 3 % 4)            #输出 "公鸡数" 100-  (25*3)对于模4 求余数
 
 print ("Now I will count the eggs:")
 
-print (3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+print (3 + 2 + 1 - 5 + 4 % 2 - 1 // 4 + 6)       #输出 3+2+1+5+ 4对于模2 求余数  -1/4 +6
 
 print ("Is it true that 3 + 2 < 5 - 7?")
 
-print (3 + 2 <5 - 7)
+print (3 + 2 <5 - 7)                            #输出 判断 3+2 < 5-7
 
-print ("What is 3 + 2?",3 + 2)
-print ("What is 5 - 7?",5 - 7)
+print ("What is 3 + 2?",3 + 2)                  #输出 3+2  的值
+print ("What is 5 - 7?",5 - 7)                  #输出 5-7  的值
 
 print ("Oh,that's why it's False.")
 
 print ("How about some more.")
 
-print ("Is it greater?",5 > -2)
-print ("Is it greater or equal",5 >= -2)
-print ("Is it less or equal?", 5 <= -2)
+print ("Is it greater?",5 > -2)                 #判断5 > -2
+print ("Is it greater or equal",5 >= -2)        #判断5 >= -2
+print ("Is it less or equal?", 5 <= -2)         #判断5 <= -2
